b2539fc8f2b690ba3b78f143607eb63c0d5023ef
MorrisHHS <23085975@student.hhs.nl>
2024-12-11 13:54:42 +0100

    error opgelost

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import InlogPagina from "./Login/InlogPagina.jsx";
 import Registreren from "./Login/Registreren.jsx";
 import KiesGebruiker from "./Login/KiesGebruiker.jsx";
@@ -11,23 +11,26 @@ function App() {
     return (
         <Router>
             <div className="App">
+                <nav>
+                    <ul>
+                    </ul>
+                </nav>
+
                 <Routes>
                     {/* Redirect van de root naar de InlogPagina */}
                     <Route path="/" element={<Navigate to="/InlogPagina" />} />
-
-                    {/* Directe routes zonder tussenliggende stappen */}
+                    <Route path="/InlogPagina/Registreren" element={<Navigate to="/Registreren" />} />
+                    <Route path="/Registreren/InlogPagina" element={<Navigate to="/InlogPagina" />} />
+                    <Route path={"/InlogPagina/KiesGebruiker"} element={<Navigate to="/KiesGebruiker" />} />
+                    <Route path={"/KiesGebruiker/RegistreerPersoon"} element={<Navigate to="/Registreren" />} />
+                    <Route path={"/KiesGebruiker/RegistreerBedrijf"} element={<Navigate to="/RegistreerBedrijf" />} />
                     <Route path="/InlogPagina" element={<InlogPagina />} />
+                    <Route path="/RegistreerBedrijf" element={<RegistreerBedrijf />} />
                     <Route path="/Registreren" element={<Registreren />} />
                     <Route path="/KiesGebruiker" element={<KiesGebruiker />} />
-                    <Route path="/RegistreerBedrijf" element={<RegistreerBedrijf />} />
-                    <Route path="/HoofdsschermMedewerker" element={<HoofdschermMedewerker />} />
+                    <Route path="/HoofdsschermMedewerker" element={<HoofdschermMedewerker/>} />
                     <Route path="/HoofdsschermParticulier" element={<HoofdschermParticulier />} />
                     <Route path="/HoofdsschermZakelijk" element={<HoofdschermZakelijk />} />
-
-                    {/* Redirects voor de juiste navigatie */}
-                    <Route path="/InlogPagina/Registreren" element={<Navigate to="/Registreren" />} />
-                    <Route path="/KiesGebruiker/RegistreerPersoon" element={<Navigate to="/Registreren" />} />
-                    <Route path="/KiesGebruiker/RegistreerBedrijf" element={<Navigate to="/RegistreerBedrijf" />} />
                 </Routes>
             </div>
         </Router>
