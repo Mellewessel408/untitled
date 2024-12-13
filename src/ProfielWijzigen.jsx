0a02036264096b72112206781af15be39c3ee1ed
Mark Wilbrink <23050063@student.hhs.nl>
2024-12-13 10:51:33 +0100

    Profielwijzigen css en jsx aangemaakt en const toegevoegd

---
+++ src/ProfielWijzigen.jsx
@@ -0,0 +1,19 @@
+﻿import React from "react";
+import './ProfielWijzigen.css';
+
+function ProfielWijzigen(){
+
+    const Wijzigen = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const naam = formData.get('naam');
+        const email = fromData.get('email');
+        const telefoonnummer = formData.get('telefoonnummer');
+        const postcode = formData.get('postcode');
+        const huisnummer = formData.get('Huisnummer');
+        const wachtwoord = formData.get('wachtwoord');
+        const herhaalwachtwoord = formData.get('herhaalwachtwoord');
+    }
+}
+export default ProfielWijzigen;
