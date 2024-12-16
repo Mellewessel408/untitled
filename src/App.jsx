7a27dcefd9cbbc0b257f48c52b832c651f58ca71
MorrisHHS <23085975@student.hhs.nl>
2024-12-16 15:57:58 +0100

    Reserveringen (werkt nog niet)

--- src/App.jsx
+++ src/App.jsx
@@ -16,44 +16,46 @@ import {AccountProvider} from "./Login/AccountProvider.jsx";
 import VoertuigselectieFrontoffice from "./Frontoffice/VoertuigselectieFrontoffice.jsx";
 import VoertuigenSelectieZakelijkHuurder from "./ZakelijkHuurder/VoertuigenSelectieZakelijkHuurder.jsx";
 import HoofdschermZakelijkHuurder from "./ZakelijkHuurder/HoofdschermZakelijkHuurder.jsx";
+import Reserveringen from "./Particulier/Reserveringen";
 
 function App() {
     return (
         <AccountProvider>
-          <Router>
-            <div className="App">
-                <nav>
-                    <ul>
-                    </ul>
-                </nav>
+            <Router>
+                <div className="App">
+                    <nav>
+                        <ul>
+                        </ul>
+                    </nav>
 
-                <Routes>
-                    {/* Redirect van de root naar de InlogPagina */}
-                    <Route path="/" element={<Navigate to="/InlogPagina" />} />
-                    <Route path="/InlogPagina/Registreren" element={<Navigate to="/Registreren" />} />
-                    <Route path="/Registreren/InlogPagina" element={<Navigate to="/InlogPagina" />} />
-                    <Route path={"/InlogPagina/KiesGebruiker"} element={<Navigate to="/KiesGebruiker" />} />
-                    <Route path={"/KiesGebruiker/RegistreerPersoon"} element={<Navigate to="/Registreren" />} />
-                    <Route path={"/KiesGebruiker/RegistreerBedrijf"} element={<Navigate to="/RegistreerBedrijf" />} />
-                    <Route path="/InlogPagina" element={<InlogPagina />} />
-                    <Route path="/RegistreerBedrijf" element={<RegistreerBedrijf />} />
-                    <Route path="/Registreren" element={<Registreren />} />
-                    <Route path="/KiesGebruiker" element={<KiesGebruiker />} />
-                    <Route path="/HoofdschermMedewerker" element={<HoofdschermMedewerker/>} />
-                    <Route path="/HoofdschermParticulier" element={<HoofdschermParticulier />} />
-                    <Route path="/HoofdschermFrontoffice" element={<HoofdschermFrontoffice />} />
-                    <Route path="/HoofdschermZakelijkBeheerder" element={<HoofdschermZakelijkBeheerder />} />
-                    <Route path="/HoofdschermZakelijkBeheerder/VoegMedewerkerToe" element={<VoegMedewerkerToe />} />
-                    <Route path="/HoofdschermZakelijkBeheerder/MedewerkerVerwijderen" element={<MedewerkerVerwijderen />} />
-                    <Route path="/HoofdschermZakelijkHuurder" element={<HoofdschermZakelijkHuurder />} />
-                    <Route path="/VoertuigenSelectie" element={<VoertuigenSelectie />} />
-                    <Route path="/VoertuigenSelectieFrontoffice" element={<VoertuigselectieFrontoffice />} />
-                    <Route path="/VoertuigenSelectieZakelijkHuurder" element={<VoertuigenSelectieZakelijkHuurder />} />
-                    <Route path="*" element={<Pagina404 />}/>
-                    <Route path="/ProfielParticulier" element={<ProfielWijzigen />} />
-                </Routes>
-            </div>
-          </Router>
+                    <Routes>
+                        {/* Redirect van de root naar de InlogPagina */}
+                        <Route path="/" element={<Navigate to="/InlogPagina" />} />
+                        <Route path="/InlogPagina/Registreren" element={<Navigate to="/Registreren" />} />
+                        <Route path="/Registreren/InlogPagina" element={<Navigate to="/InlogPagina" />} />
+                        <Route path={"/InlogPagina/KiesGebruiker"} element={<Navigate to="/KiesGebruiker" />} />
+                        <Route path={"/KiesGebruiker/RegistreerPersoon"} element={<Navigate to="/Registreren" />} />
+                        <Route path={"/KiesGebruiker/RegistreerBedrijf"} element={<Navigate to="/RegistreerBedrijf" />} />
+                        <Route path="/InlogPagina" element={<InlogPagina />} />
+                        <Route path="/RegistreerBedrijf" element={<RegistreerBedrijf />} />
+                        <Route path="/Registreren" element={<Registreren />} />
+                        <Route path="/KiesGebruiker" element={<KiesGebruiker />} />
+                        <Route path="/HoofdschermMedewerker" element={<HoofdschermMedewerker/>} />
+                        <Route path="/HoofdschermParticulier" element={<HoofdschermParticulier />} />
+                        <Route path="/HoofdschermFrontoffice" element={<HoofdschermFrontoffice />} />
+                        <Route path="/HoofdschermZakelijkBeheerder" element={<HoofdschermZakelijkBeheerder />} />
+                        <Route path="/HoofdschermZakelijkBeheerder/VoegMedewerkerToe" element={<VoegMedewerkerToe />} />
+                        <Route path="/HoofdschermZakelijkBeheerder/MedewerkerVerwijderen" element={<MedewerkerVerwijderen />} />
+                        <Route path="/HoofdschermZakelijkHuurder" element={<HoofdschermZakelijkHuurder />} />
+                        <Route path="/VoertuigenSelectie" element={<VoertuigenSelectie />} />
+                        <Route path="/VoertuigenSelectieFrontoffice" element={<VoertuigselectieFrontoffice />} />
+                        <Route path="/VoertuigenSelectieZakelijkHuurder" element={<VoertuigenSelectieZakelijkHuurder />} />
+                        <Route path="/Reserveringen" element={<Reserveringen />} />
+                        <Route path="*" element={<Pagina404 />}/>
+                        <Route path="/ProfielParticulier" element={<ProfielWijzigen />} />
+                    </Routes>
+                </div>
+            </Router>
         </AccountProvider>
     );
 }
